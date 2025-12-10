0d2aa8d17f1b57c14158bf294a2208329fa9fba3
Yannick Bazan <yannick.bazan@by-devinnovation.com>
2025-12-10 10:22:59 -0500

    (feat) Enables custom origin configuration
    - Introduces configuration option for allowed origin.
    - Allows to specify a custom origin for CORS configuration.
    - Defaults to allowing all origins if no configuration is provided.

--- surfsense_backend/app/app.py
+++ surfsense_backend/app/app.py
@@ -26,6 +26,10 @@ def registration_allowed():
         )
     return True
 
+def allowed_origin():
+    if config.ALLOWED_ORIGIN:
+        return config.ALLOWED_ORIGIN
+    return "*"
 
 app = FastAPI(lifespan=lifespan)
 
@@ -36,7 +40,7 @@ app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=[allowed_origin],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
